78a516bdcecdab9915c2cada87bd6e720619387e
NITISH TOM MICHAEL <niteshmichael@gmail.com>
2021-09-08 16:14:00 +0530

    Update pymysql_connection.py

--- pymysql_connection.py
+++ pymysql_connection.py
@@ -1,1 +1,10 @@
 
+from sqlalchemy.orm import sessionmaker
+
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://nitish:password@localhost/mydb'
+
+
+# Test if it works
+engine = sqlalchemy.create_engine(SQLALCHEMY_DATABASE_URI, echo=True)
+Session = sessionmaker(bind=engine)
+session = Session()
